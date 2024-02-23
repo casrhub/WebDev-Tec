bc70096c4949fb8749c78d4a7ed8108ccc87b707
César Silva <casrr@proton.me>
2024-02-23 14:33:53 -0600

    Added Space X endpoint: https://api.spacexdata.com/v4/rockets/$\{rocketId\}

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-function miFuncion(){
-    const endpoint = "https://en.wikipedia.org/w/rest.php/v1/search/title?q=ball&limit=5"
-    fetch(endpoint).then(
-        function(response){
-            // pelamos la mandarina (procesamos JSON)
-            return response.json();
-        }
-    ).then (
-        // Entregamos la mandarina pelada 
-        function (j){
-            console.log(j)
-            
-        }
+// function miFuncion(){
+//     const OpenAiEndpoint = "https://api.openai.com/v1/chat/completions";
+//     const opciones = {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json','Authorization': 'Bearer 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' },
+//         body: JSON.stringify({ model: 'gpt-3.5-turbo', messages: [{"role": "user", "content": "Say two in spanish!"}] })
+//     };
+//     fetch(OpenAiEndpoint,opciones).then(
+//         function(response){
+//             // pelamos la mandarina (procesamos JSON)
+//             return response.json();
+//         }
+//     ).then (
+//         // Entregamos la mandarina pelada 
+//         function (j){
+//             console.log(j.choices[0].message.content)
+//         }
+//     )
+// }
+ 
 
-    )
+
+
+function useSpaceXandOpenAI(){
+    const rocketId = document.getElementById('rocketIdInput').value;
+    const apiUrl = `https://api.spacexdata.com/v4/rockets/${rocketId}`;
+    axios.get(apiUrl)
+  .then(response => {
+    // Process the data received from the SpaceX API
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error.message);
+  });
+
 }
+
+
+
+
